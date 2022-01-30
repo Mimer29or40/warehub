86ef54d18a264453bb3ed3d1d2eb107e4e98b53b
Ryan <>
2022-01-30 14:15:41 -0500

    Revamped run configurations. Added generate call to add

--- test/delete_test_data.py
+++ test/delete_test_data.py
@@ -2,11 +2,11 @@ from pathlib import Path
 
 
 def main():
-    test_data_dir: Path = Path('test_data')
+    test_data_dir: Path = Path("test_data")
 
-    data_file = test_data_dir / 'data.json'
+    data_file = test_data_dir / "data.json"
     data_file.unlink(missing_ok=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
